86a4bf2e1c28250dbb4123726af2aef89c79d525
walker <orzwalker@gmail.com>
2022-09-14 16:30:57 +0800

    解决相互并发转账【活锁】问题

--- main/java/com/lock/Account.java
+++ main/java/com/lock/Account.java
@@ -1,6 +1,11 @@
 package com.lock;
 
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 串行化转账
  */
@@ -48,4 +53,61 @@ public class Account {
             }
         }
     }
+
+    // ============================
+    Lock lock = new ReentrantLock();
+
+    void transferV3(Account target, int num) {
+        while (true) {
+            // 非阻塞式获取锁，不会出现死锁，但是会活锁（非阻塞式死锁）
+            if (this.lock.tryLock()) { // T1、T2线程相互转账，第一次获取自己的锁都成功了
+                try {
+                    if (target.lock.tryLock()) { // 获取对方锁时，失败，都释放了锁资源，while循环一直在尝试获取锁
+                        try {
+                            if (this.balance > num) {
+                                this.balance -= num;
+                                target.balance += num;
+                            }
+                        } finally {
+                            target.lock.unlock();
+                        }
+                    }
+                } finally {
+                    this.lock.unlock();
+                }
+            }
+        }
+
+    }
+
+    void transferV3Deal(Account target, int num) {
+        int max = 100, min = 1;
+        while (true) {
+            // 非阻塞式获取锁，不会出现死锁，但是会活锁（非阻塞式死锁）
+            if (this.lock.tryLock()) { // T1、T2线程相互转账，第一次获取自己的锁都成功了
+                try {
+                    // 获取第二把锁前随机休眠一段时间，防止活锁
+                    int waitTime = ThreadLocalRandom.current().nextInt(min, max + 1);
+                    TimeUnit.MILLISECONDS.sleep(waitTime);
+                    if (target.lock.tryLock()) { // 获取对方锁时，失败，都释放了锁资源，while循环一直在尝试获取锁
+                        try {
+                            if (this.balance > num) {
+                                this.balance -= num;
+                                target.balance += num;
+                            }
+                        } finally {
+                            target.lock.unlock();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                } finally {
+                    this.lock.unlock();
+                }
+            }
+        }
+
+    }
 }
+
