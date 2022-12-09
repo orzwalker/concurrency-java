0474d373e6c3efd6280e2ec07aab8d69c094e424
walker <orzwalker@gmail.com>
2022-12-09 17:50:16 +0800

    3种TL线程变量存储

---
+++ main/java/com/tl/ThreadLocalTest.java
@@ -0,0 +1,113 @@
+package com.tl;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+import com.alibaba.ttl.TtlRunnable;
+import com.alibaba.ttl.threadpool.TtlExecutors;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 测试引入线程池后，标识符在父子线程之间复制的问题
+ *
+ * @author walker
+ * @since 2022/6/10 08:54
+ */
+public class ThreadLocalTest {
+
+
+    /*
+     * Thread-1:1
+     * Thread-3:3
+     * Thread-0:0
+     * Thread-2:2
+     * Thread-4:4
+     * parent:Thread-0:null
+     * parent:Thread-3:null
+     * parent:Thread-1:null
+     * parent:Thread-4:null
+     * parent:Thread-2:null
+     */
+//    private static final ThreadLocal<Integer> TL = new ThreadLocal<>();
+
+    /*
+     * Thread-0:0
+     * Thread-3:3
+     * Thread-1:1
+     * Thread-2:2
+     * Thread-4:4
+     * parent:Thread-3:3
+     * parent:Thread-4:3
+     * parent:Thread-0:3
+     * parent:Thread-1:1
+     * parent:Thread-2:2
+     */
+//    private static final ThreadLocal<Integer> TL = new InheritableThreadLocal<>();
+
+    /*
+     * Thread-1:0
+     * Thread-4:3
+     * Thread-5:4
+     * Thread-2:1
+     * Thread-3:2
+     * parent:Thread-5:4
+     * parent:Thread-1:0
+     * parent:Thread-4:3
+     * parent:Thread-3:2
+     * parent:Thread-2:1
+     */
+    private static final ThreadLocal<Integer> TL = new TransmittableThreadLocal<>();
+
+    static ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(3);
+    // 模拟Tomcat线程池
+    private static final ExecutorService threadPool = TtlExecutors.getTtlExecutorService(threadPoolExecutor);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new TomcatThread(i).start();
+        }
+    }
+
+
+    /**
+     * 父线程----提交任务的线程
+     */
+    static class TomcatThread extends Thread {
+        private final int index;
+
+        public TomcatThread(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public void run() {
+            // 父线程将标识添加到线程上下文变量TL
+            TL.set(index);
+            String name = Thread.currentThread().getName();
+            System.out.println(name + ":" + index);
+            BusinessTask businessTask = new BusinessTask(name);
+            // 提交到线程池，线程池执行业务任务
+            assert threadPool != null;
+            // 线程池工作线程拿到任务，开始执行
+            threadPool.execute(Objects.requireNonNull(TtlRunnable.get(businessTask)));
+        }
+    }
+
+    /**
+     * 业务任务
+     */
+    static class BusinessTask implements Runnable {
+        private final String parentThreadName;
+
+        public BusinessTask(String parentThreadName) {
+            this.parentThreadName = parentThreadName;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("parent:" + parentThreadName + ":" + TL.get());
+        }
+    }
+
+}
---
+++ main/java/com/tl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * ThreadLocal存储线程变量
+ *
+ * @author walker
+ * @since 2022/12/9 17:31
+ */
+package com.tl;
